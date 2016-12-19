41e1c55f7222cdae625283dca945eadb875249d2
fuyuguang <2858311356@qq.com>
2016-12-19 09:18:03 +0800

    update

--- app/src/main/java/com/jiayou/fyg/hellolibrary/MainActivity.java
+++ app/src/main/java/com/jiayou/fyg/hellolibrary/MainActivity.java
@@ -1,9 +1,8 @@
 package com.jiayou.fyg.hellolibrary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import com.icodeyou.library.CommonAdapter;
 import com.jiayou.fyg.jiuxiantoollib.JiuxianUtil;
 
 public class MainActivity extends AppCompatActivity {
