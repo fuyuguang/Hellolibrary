c7f6413798ab5df3929c8dedd935e7e405f2526b
fuyuguang <2858311356@qq.com>
2017-01-05 22:19:45 +0800

    init feature 2

--- app/src/main/java/com/jiayou/fyg/hellolibrary/MainActivity.java
+++ app/src/main/java/com/jiayou/fyg/hellolibrary/MainActivity.java
@@ -2,10 +2,16 @@ package com.jiayou.fyg.hellolibrary;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.jiayou.fyg.jiuxiantoollib.JiuxianUtil;
 
-public class MainActivity extends AppCompatActivity {
+import cn.trinea.android.common.util.ToastUtils;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button feature_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +21,20 @@ public class MainActivity extends AppCompatActivity {
 
         JiuxianUtil.getAppName();
 
+        feature_2 = (Button) findViewById(R.id.feature_2);
+        feature_2.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+
+            case R.id.feature_2:
+
+                ToastUtils.show(this," 我是 feature 2 ");
+                break;
+        }
     }
 }
