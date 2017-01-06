5f18d721dfb70e96c0476b1b5c75fc1c2ea0e3a0
fuyuguang <2858311356@qq.com>
2017-01-06 18:29:23 +0800

    add  feature 11 note

--- app/src/main/java/com/jiayou/fyg/hellolibrary/MainActivity.java
+++ app/src/main/java/com/jiayou/fyg/hellolibrary/MainActivity.java
@@ -38,6 +38,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //TODO 添加 feature 10 的功能
 
+        //TODO 添加 feature 11 的功能
+
         feature_1 = (Button) findViewById(R.id.feature_1);
         feature_1.setOnClickListener(this);
         feature_2 = (Button) findViewById(R.id.feature_2);
