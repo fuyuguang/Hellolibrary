15c5f005556c99e55a0db1e81665d084c7101ad2
fuyuguang <2858311356@qq.com>
2017-01-06 15:41:28 +0800

    add feature 9  feature

--- app/src/main/java/com/jiayou/fyg/hellolibrary/MainActivity.java
+++ app/src/main/java/com/jiayou/fyg/hellolibrary/MainActivity.java
@@ -34,6 +34,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         JiuxianUtil.getAppName();
 
 
+        //TODO 添加 feature 9 的功能
+
         feature_1 = (Button) findViewById(R.id.feature_1);
         feature_1.setOnClickListener(this);
         feature_2 = (Button) findViewById(R.id.feature_2);
