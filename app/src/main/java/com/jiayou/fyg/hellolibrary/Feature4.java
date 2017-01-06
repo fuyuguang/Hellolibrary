8e5cb002a2fe34592178f33819dbd68c8726539f
fuyuguang <2858311356@qq.com>
2017-01-06 11:35:39 +0800

    Revert "Revert "add feature 4   in  dev""
    This reverts commit 7bd1780e86a75456db5164130a813fc02904190e.

---
+++ app/src/main/java/com/jiayou/fyg/hellolibrary/Feature4.java
@@ -0,0 +1,42 @@
+package com.jiayou.fyg.hellolibrary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.jiayou.fyg.jiuxiantoollib.JiuxianUtil;
+
+import cn.trinea.android.common.util.ToastUtils;
+
+
+public class Feature4 extends AppCompatActivity implements View.OnClickListener {
+
+    private Button feature_3_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feature3_activity);
+
+        feature_3_btn = (Button) findViewById(R.id.feature_3_btn);
+        feature_3_btn.setOnClickListener(this);
+
+        JiuxianUtil.getAppName();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case  R.id.feature_3_btn:
+
+                ToastUtils.show(this, " feature 4 界面  ，我是功能 4  按钮 ");
+
+            break;
+
+        }
+    }
+
+}
